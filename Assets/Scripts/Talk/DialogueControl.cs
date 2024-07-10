17c350bb23b435a4c186f1fe0c494db4cad225b9
NewbeCat <catonamat098@gmail.com>
2024-07-10 21:37:51 +0900

    UI fix
    -Dialogue system_on going
    -UI Tweening_finished

--- Assets/Scripts/Talk/DialogueControl.cs
+++ Assets/Scripts/Talk/DialogueControl.cs
@@ -3,29 +3,33 @@ using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
-    public void SetDialogue(TextMeshProUGUI dialogueTextUI, DialogueObject dialogueObject, int index)
+    [SerializeField] private TextMeshProUGUI dialogueNameUI;
+    [SerializeField] private TextMeshProUGUI dialogueTextUI;
+    public void SetDialogue(DialogueObject dialogueObject, int index)
     {
+        //만약 현재 페이지 안 켜졌을시 켜기
         if (index < 0 || index >= dialogueObject.dialogue.Count)
         {
             Debug.LogWarning("Index out of range.");
             return;
         }
-
+        dialogueNameUI.text = dialogueObject.dialogue[index].name;
         dialogueTextUI.text = dialogueObject.dialogue[index].dialogueText;
     }
 
-    public void SetNextDialogue(TextMeshProUGUI dialogueTextUI, DialogueObject dialogueObject, ref int currentIndex)
+    public void SetNextDialogue(DialogueObject dialogueObject, ref int currentIndex)
     {
         currentIndex++;
         if (currentIndex >= dialogueObject.dialogue.Count)
         {
             currentIndex = 0; // Reset to first dialogue or handle end of dialogues
+            //현재 페이지 닫기
         }
 
-        SetDialogue(dialogueTextUI, dialogueObject, currentIndex);
+        SetDialogue(dialogueObject, currentIndex);
     }
 
-    public void SetPreviousDialogue(TextMeshProUGUI dialogueTextUI, DialogueObject dialogueObject, ref int currentIndex)
+    public void SetPreviousDialogue(DialogueObject dialogueObject, ref int currentIndex)
     {
         currentIndex--;
         if (currentIndex < 0)
@@ -33,6 +37,6 @@ public class DialogueManager : MonoBehaviour
             currentIndex = dialogueObject.dialogue.Count - 1; // Set to last dialogue or handle start of dialogues
         }
 
-        SetDialogue(dialogueTextUI, dialogueObject, currentIndex);
+        SetDialogue(dialogueObject, currentIndex);
     }
 }
---
+++ Assets/Scripts/UI/OpenCloseWindow.cs
@@ -0,0 +1,174 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class OpenCloseWindow : MonoBehaviour
+{
+    [Header("Window Setup")]
+    [SerializeField] private GameObject window;
+
+    [SerializeField] private RectTransform windowRectTransform;
+    [SerializeField] private CanvasGroup windowCanvasGroup;
+
+    public enum AnimateToDirection
+    {
+        Top,
+        Bottom,
+        Left,
+        Right
+    }
+
+    [Header("Animation Setup")]
+    [SerializeField] private AnimateToDirection openDirection = AnimateToDirection.Top;
+    [SerializeField] private AnimateToDirection closeDirection = AnimateToDirection.Bottom;
+    [Space]
+    [SerializeField] private Vector2 distanceToAnimate = new Vector2(100, 100);
+    [SerializeField] private AnimationCurve easingCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    [Range(0, 1f)][SerializeField] private float animationDuration = 0.5f;
+
+    private bool _isOpen;
+    private Vector2 _initialPosition;
+    private Vector2 _currentPosition;
+
+    private Vector2 _upOffset;
+    private Vector2 _downOffset;
+    private Vector2 _leftOffset;
+    private Vector2 _rightOffset;
+
+    private Coroutine _animateWindowCoroutine;
+
+    public static event Action OnOpenWindow;
+    public static event Action OnCloseWindow;
+
+
+
+    private void OnValidate()
+    {
+        if (window != null)
+        {
+            windowRectTransform = window.GetComponent<RectTransform>();
+            windowCanvasGroup = window.GetComponent<CanvasGroup>();
+        }
+
+        distanceToAnimate.x = Mathf.Max(0, distanceToAnimate.x);
+        distanceToAnimate.y = Mathf.Max(0, distanceToAnimate.y);
+
+
+        _initialPosition = window.transform.position;
+    }
+
+    #region AnimationFunctionality
+
+    private void Start()
+    {
+        _initialPosition = window.transform.position;
+
+        InitializeOffsetPositions();
+
+        ToggleOpenClose();
+    }
+
+    private void InitializeOffsetPositions()
+    {
+        _upOffset = new Vector2(0, distanceToAnimate.y);
+        _downOffset = new Vector2(0, -distanceToAnimate.y);
+
+        _rightOffset = new Vector2(+distanceToAnimate.x, 0);
+        _leftOffset = new Vector2(-distanceToAnimate.x, 0);
+    }
+    public void ToggleOpenClose()
+    {
+        if (_isOpen)
+            CloseWindow();
+        else
+            OpenWindow();
+    }
+    public void OpenWindow()
+    {
+        if (_isOpen)
+            return;
+
+        _isOpen = true;
+        OnOpenWindow?.Invoke();
+
+        if (_animateWindowCoroutine != null)
+            StopCoroutine(_animateWindowCoroutine);
+
+        _animateWindowCoroutine = StartCoroutine(AnimateWindow(true));
+    }
+    public void CloseWindow()
+    {
+        if (!_isOpen)
+            return;
+
+        _isOpen = false;
+        OnCloseWindow?.Invoke();
+
+        if (_animateWindowCoroutine != null)
+            StopCoroutine(_animateWindowCoroutine);
+
+        _animateWindowCoroutine = StartCoroutine(AnimateWindow(false));
+    }
+
+    private Vector2 GetOffset(AnimateToDirection direction)
+    {
+        switch (direction)
+        {
+            case AnimateToDirection.Top:
+                return _upOffset;
+            case AnimateToDirection.Bottom:
+                return _downOffset;
+            case AnimateToDirection.Left:
+                return _leftOffset;
+            case AnimateToDirection.Right:
+                return _rightOffset;
+            default:
+                return Vector3.zero;
+        }
+    }
+
+    private IEnumerator AnimateWindow(bool open)
+    {
+        if (open)
+            window.gameObject.SetActive(true);
+
+        _currentPosition = window.transform.position;
+
+        float elapsedTime = 0;
+
+        Vector2 targetPosition = _currentPosition;
+
+        if (open)
+            targetPosition = _currentPosition + GetOffset(openDirection);
+        else
+            targetPosition = _currentPosition + GetOffset(closeDirection);
+
+        while (elapsedTime < animationDuration)
+        {
+            float evaluationAtTime = easingCurve.Evaluate(elapsedTime / animationDuration);
+
+            window.transform.position = Vector2.Lerp(_currentPosition, targetPosition, evaluationAtTime);
+
+            windowCanvasGroup.alpha = open
+                ? Mathf.Lerp(0f, 1f, evaluationAtTime)
+                : Mathf.Lerp(1f, 0f, evaluationAtTime);
+
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        window.transform.position = targetPosition;
+
+        windowCanvasGroup.alpha = open ? 1 : 0;
+        windowCanvasGroup.interactable = open;
+        windowCanvasGroup.blocksRaycasts = open;
+
+        if (!open)
+        {
+            window.gameObject.SetActive(false);
+            window.transform.position = _initialPosition;
+        }
+    }
+
+    #endregion
+}
