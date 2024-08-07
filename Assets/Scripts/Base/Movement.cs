fa2f4cc2ed5b924a9fa867e54f336b97fb123fff
NewbeCat <catonamat098@gmail.com>
2024-08-07 18:41:35 +0900

    base player

---
+++ Assets/Scripts/Base/Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    //regular movement
+    void moveUp()
+    {
+
+    }
+    void moveDown()
+    {
+
+    }
+    void moveLeft()
+    {
+
+    }
+    void moveRight()
+    {
+
+    }
+}
