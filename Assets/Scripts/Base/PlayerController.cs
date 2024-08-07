da59abd1bbee1aa83f1e948b662cce0422741c05
NewbeCat <catonamat098@gmail.com>
2024-08-08 01:24:15 +0900

    Walking Animation

--- Assets/Scripts/Base/PlayerController.cs
+++ Assets/Scripts/Base/PlayerController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed;
     private bool isMoving;
     private Vector2 input;
+
     private void Update()
     {
         if (!isMoving)
@@ -14,16 +16,21 @@ public class PlayerController : MonoBehaviour
             input.x = Input.GetAxisRaw("Horizontal");
             input.y = Input.GetAxisRaw("Vertical");
 
+            //if (input.x != 0) input.y = 0; //십자로만 움직이게 끊어내기
+
             if (input != Vector2.zero)
             {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
                 var targetPos = transform.position;
                 targetPos.x += input.x;
                 targetPos.y += input.y;
-
                 StartCoroutine(Move(targetPos));
             }
         }
 
+        animator.SetBool("isMoving", isMoving);
 
     }
 
@@ -37,7 +44,6 @@ public class PlayerController : MonoBehaviour
             yield return null;
         }
         transform.position = targetPos;
-
         isMoving = false;
     }
 }
